5230b03880e0988177e2f084b6d006aef173df71
baexxbin <soo3886@gmail.com>
2024-05-23 22:42:51 +0900

    chore: initial project setup

---
+++ src/main/java/com/baexxbin/wishrise/WishriseApplication.java
@@ -0,0 +1,13 @@
+package com.baexxbin.wishrise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishriseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baexxbin/wishrise/WishriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baexxbin.wishrise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
